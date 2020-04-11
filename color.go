043129a2680677c0bc57319e20875781078ed5a4
ermanimer <ermanimer@gmail.com>
2020-04-11 20:22:15 +0300

    Returns only colored text

--- color.go
+++ color.go
@@ -4,48 +4,44 @@ import "fmt"
 
 //colors
 const (
-	black   = "\u001b[30m%s\u001b[0m%s"
-	red     = "\u001b[31m%s\u001b[0m%s"
-	green   = "\u001b[32m%s\u001b[0m%s"
-	yellow  = "\u001b[33m%s\u001b[0m%s"
-	blue    = "\u001b[34m%s\u001b[0m%s"
-	magenta = "\u001b[35m%s\u001b[0m%s"
-	cyan    = "\u001b[36m%s\u001b[0m%s"
-	white   = "\u001b[37m%s\u001b[0m%s"
+	black   = "\u001b[30m%s\u001b[0m"
+	red     = "\u001b[31m%s\u001b[0m"
+	green   = "\u001b[32m%s\u001b[0m"
+	yellow  = "\u001b[33m%s\u001b[0m"
+	blue    = "\u001b[34m%s\u001b[0m"
+	magenta = "\u001b[35m%s\u001b[0m"
+	cyan    = "\u001b[36m%s\u001b[0m"
+	white   = "\u001b[37m%s\u001b[0m"
 )
 
-func Black(text string) {
-	print(black, text)
+func Black(text string) string {
+	return fmt.Sprintf(black, text)
 }
 
-func Red(text string) {
-	print(red, text)
+func Red(text string) string {
+	return fmt.Sprintf(red, text)
 }
 
-func Green(text string) {
-	print(green, text)
+func Green(text string) string {
+	return fmt.Sprintf(green, text)
 }
 
-func Yellow(text string) {
-	print(yellow, text)
+func Yellow(text string) string {
+	return fmt.Sprintf(yellow, text)
 }
 
-func Blue(text string) {
-	print(blue, text)
+func Blue(text string) string {
+	return fmt.Sprintf(blue, text)
 }
 
-func Magenta(text string) {
-	print(magenta, text)
+func Magenta(text string) string {
+	return fmt.Sprintf(magenta, text)
 }
 
-func Cyan(text string) {
-	print(cyan, text)
+func Cyan(text string) string {
+	return fmt.Sprintf(cyan, text)
 }
 
-func White(text string) {
-	print(white, text)
-}
-
-func print(color string, text string) {
-	fmt.Printf(color, text)
+func White(text string) string {
+	return fmt.Sprintf(white, text)
 }
